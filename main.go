c80e333197eec04ce64b4f35ccb008242dec9f8c
alu <thatch04@163.com>
2023-11-01 10:06:38 +0800

    目录更改

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"RpcProvider/handler"
 	service "RpcProvider/kitex_gen/service/provider"
 	"log"
 )
 
 func main() {
-	svr := service.NewServer(new(ProviderImpl))
+	svr := service.NewServer(new(handler.ProviderImpl))
 
 	err := svr.Run()
 
